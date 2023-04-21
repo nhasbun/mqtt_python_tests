af58c0ff58176dded2052089db1ed5da35113b05
Nicolás Hasbún A <nhasbun@gmail.com>
2023-04-21 19:20:20 -0400

    Improve pub/sub behavior: reconnect, package logs

--- publisher.py
+++ publisher.py
@@ -1,24 +1,62 @@
-import paho.mqtt.client as mqtt
-import time
 import argparse
 from datetime import datetime
+from threading import Event
+from time import sleep
+
+import paho.mqtt.client as mqtt
+
+
+# Parameters
+qos_level = 2
+
+
+# Global variables
+stop_flag = Event()
+
 
 # Parse the command-line arguments
+# Expected argument of broker ip address
 parser = argparse.ArgumentParser(description="MQTT publisher")
 parser.add_argument("broker", type=str, help="The address of the MQTT broker")
 args = parser.parse_args()
 
+
+# MQTT Client callbacks
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected to {args.broker}...")
+
+
+def on_disconnect(client, userdata, rc):
+    print(f"Disconnected to {args.broker}...")
+
+
+def on_log(client, userdata, level, buf):
+    print(f"Log: {buf}")
+
+
 # Configure the MQTT client
 client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_log = on_log
 client.connect(args.broker, 1883, 60)
+"""This handles re-connection automatically including offline buffering.
 
-# Publish a message every second with QoS level 2 and offline buffering
-while True:
+Check: https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php#network-loop
+"""
+client.loop_start()
+
+
+# Publish a message every second with QoS level 2
+while not stop_flag.wait(1.0):
+
+    print(f"Client is connected: {client.is_connected()}")
+
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = f"Hello World! {timestamp}"
-    (rc, mid) = client.publish("topic_test", message, qos=2, retain=True)
+    (rc, mid) = client.publish("topic_test", message, qos=qos_level)
     if rc != mqtt.MQTT_ERR_SUCCESS:
         print(f"Error publishing message: {rc}")
     else:
         print(f"Published message: {message}")
-    time.sleep(1)
+
--- subscriber.py
+++ subscriber.py
@@ -1,30 +1,60 @@
+import argparse
+from time import sleep
+
 import paho.mqtt.client as mqtt
 from pynput import keyboard
 
-# Function to handle incoming messages
+# Parameters
+qos_level = 2
+
+
+# Parse the command-line arguments
+# Expected argument of broker ip address
+parser = argparse.ArgumentParser(description="MQTT publisher")
+parser.add_argument("broker", type=str, help="The address of the MQTT broker")
+args = parser.parse_args()
+
+
+# MQTT Client callbacks
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected to {args.broker}...")
+
+    # Subscribe to the MQTT topic
+    topic = "topic_test"
+    print(f"Subscribing to {topic}...")
+    client.subscribe(topic, qos=qos_level)
+
+
+def on_disconnect(client, userdata, rc):
+    print(f"Disconnected to {args.broker}...")
+
+
+def on_log(client, userdata, level, buf):
+    print(f"Log: {buf}")
+
+
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
-# Function to execute upon connecting to the MQTT broker
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Successful connection to MQTT broker")
-    else:
-        print("Error connecting to MQTT broker")
 
-# Function to execute upon subscribing to an MQTT topic
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed to MQTT topic")
+
 
 # Configure the MQTT client
 client = mqtt.Client()
 client.on_message = on_message
 client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_log = on_log
 client.on_subscribe = on_subscribe
-client.connect("localhost", 1883, 60)
+client.connect(args.broker, 1883, 60)
+"""This handles re-connection automatically including offline buffering.
 
-# Subscribe to the MQTT topic
-client.subscribe("topic_test")
+Check: https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php#network-loop
+"""
+client.loop_start()
+
 
 # Create keyboard listener
 def on_press(key):
@@ -36,9 +66,9 @@ listener.start()
 
 # Main loop of the script
 while True:
-    # Wait for incoming MQTT messages
-    client.loop()
-
+    
     # Exit the loop if the "esc" key is pressed
     if not listener.running:
         break
+
+    sleep(0.1)
