8634ee71a99e7bf8889488e5a9ccef748a6bb5d0
Nicolás Hasbún A <nhasbun@gmail.com>
2023-04-21 03:53:37 -0400

    improving timestamp readability

--- publisher.py
+++ publisher.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
 import argparse
+from datetime import datetime
 
 # Parse the command-line arguments
 parser = argparse.ArgumentParser(description="MQTT publisher")
@@ -13,9 +14,11 @@ client.connect(args.broker, 1883, 60)
 
 # Publish a message every second with QoS level 2 and offline buffering
 while True:
-    timestamp = int(time.time())
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = f"Hello World! {timestamp}"
     (rc, mid) = client.publish("topic_test", message, qos=2, retain=True)
     if rc != mqtt.MQTT_ERR_SUCCESS:
         print(f"Error publishing message: {rc}")
+    else:
+        print(f"Published message: {message}")
     time.sleep(1)
